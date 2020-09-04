7da70035ac50f13769a117d13bc632c3578ae673
何宏杰 <850595216@qq.com>
2020-09-04 17:34:09 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="home">
+        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+            <van-swipe-item>1</van-swipe-item>
+            <van-swipe-item>2</van-swipe-item>
+            <van-swipe-item>3</van-swipe-item>
+            <van-swipe-item>4</van-swipe-item>
+        </van-swipe>
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+import {Swipe,SwipeItem} from 'vant'
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+export default {
+    name:'Home',
+    components:{},
+    async created(){
+        const {data} = await this.$request.get('/goods',{
+            params:{
+                size:5,
+                sort:'sales_qty'
+            }
+        })
+    }
+}
+</script>
+<style lang='scss'>
+</style>
